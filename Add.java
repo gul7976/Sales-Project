dbcd8742598cc9978975097efc79a2f299d3184d
Gulshan Sharma <Gulshan.sharma@TL214.triconhome.local>
2024-10-24 11:01:48 +0530

    Allow sum of multiple user inputs

--- Add.java
+++ Add.java
@@ -1,16 +1,20 @@
-import java.util.Scanner; // Import the Scanner class
+import java.util.Scanner;
 
 class MyClass {
   public static void main(String[] args) {
-    int x, y, sum;
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
-    System.out.println("Type a number:");
-    x = myObj.nextInt(); // Read user input
+    int sum = 0;
+    String continueInput;
 
-    System.out.println("Type another number:");
-    y = myObj.nextInt(); // Read user input
+    do {
+      System.out.println("Type a number:");
+      int num = myObj.nextInt();  // Read user input
+      sum += num;  // Add number to sum
 
-    sum = x + y;  // Calculate the sum of x + y
-    System.out.println("Sum is: " + sum); // Print the sum
+      System.out.println("Do you want to add another number? (yes/no):");
+      continueInput = myObj.next();  // Read user choice
+    } while (continueInput.equalsIgnoreCase("yes"));
+
+    System.out.println("Total sum is: " + sum);  // Print the total sum
   }
-} 
+}
