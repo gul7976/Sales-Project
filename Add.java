38afed7bbd9d9355ae4f09c9dd65e3b2db6fafaf
Gulshan Sharma <Gulshan.sharma@TL214.triconhome.local>
2024-10-24 11:02:57 +0530

    Add subtraction functionality

--- Add.java
+++ Add.java
@@ -3,18 +3,29 @@ import java.util.Scanner;
 class MyClass {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in); // Create a Scanner object
-    int sum = 0;
+    int result = 0;
     String continueInput;
+
+    System.out.println("Choose operation: 'add' or 'subtract':");
+    String operation = myObj.next();  // Read user operation choice
 
     do {
       System.out.println("Type a number:");
       int num = myObj.nextInt();  // Read user input
-      sum += num;  // Add number to sum
+
+      if (operation.equalsIgnoreCase("add")) {
+        result += num;  // Add number to result
+      } else if (operation.equalsIgnoreCase("subtract")) {
+        result -= num;  // Subtract number from result
+      } else {
+        System.out.println("Invalid operation!");
+        return;  // Exit if invalid operation is chosen
+      }
 
       System.out.println("Do you want to add another number? (yes/no):");
       continueInput = myObj.next();  // Read user choice
     } while (continueInput.equalsIgnoreCase("yes"));
 
-    System.out.println("Total sum is: " + sum);  // Print the total sum
+    System.out.println("Final result is: " + result);  // Print the result
   }
 }
